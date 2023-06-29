ef5c9dc82921f48634528f1cb3fff54b5ecb1367
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-06-29 11:16:55 +0700

    Server and Reciever can get most Scores
    left the test for deuce, Advantage, and win game

--- tennis_20230629/spec/tennis_spec.rb
+++ tennis_20230629/spec/tennis_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe Tennis do
     {server: 2, reciever: 0, score: "30 Love"},
     {server: 3, reciever: 0, score: "40 Love"},
     {server: 0, reciever: 1, score: "Love 15"},
+    {server: 0, reciever: 2, score: "Love 30"},
+    {server: 0, reciever: 3, score: "Love 40"},
+    {server: 1, reciever: 1, score: "15 All"},
+    {server: 2, reciever: 2, score: "30 All"},
+    # {server: 3, reciever: 3, score: "Deuce"},
+    # {server: 4, reciever: 3, score: "Advantage Server"},
+    # {server: 4, reciever: 4, score: "Deuce"},
+    # {server: 4, reciever: 5, score: "Advantage Reciever"},
+    # {server: 4, reciever: 6, score: "Game Reciever"},
+    # {server: 4, reciever: 0, score: "Game Server"},
   ].each do |args|
     it "returns #{args[:score].inspect} when Server: #{args[:server]} and Reciever: #{args[:reciever]}" do
       tennis = Tennis.new
