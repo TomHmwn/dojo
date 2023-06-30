16854977df846ccf0d702708201f878d5f7e4fa4
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-06-30 14:36:12 +0700

    Allow for advantage for reciever and server
    refactor to use all? for deuce

--- tennis_20230629/lib/tennis.rb
+++ tennis_20230629/lib/tennis.rb
@@ -7,9 +7,13 @@ class Tennis
 
   def score(player = nil)
     @points[player] += 1 if player
-    if @points[:server] == @points [:reciever] && @points[:server] == 3
-      return "Deuce"
-    elsif @points[:server] == @points[:reciever]
+    if @points.values.all? { |point| point >= 3 }
+      return "Deuce" if @points[:server] == @points[:reciever]
+      return "Advantage Server" if @points[:server] > @points[:reciever]
+      return "Advantage Reciever" if @points[:server] < @points[:reciever]
+    end
+
+    if @points[:server] == @points[:reciever]
       return "#{POINT_TRANSLATOR[@points[:server]]} All"
     end
     "#{POINT_TRANSLATOR[@points[:server]]} #{POINT_TRANSLATOR[@points[:reciever]]}"
--- tennis_20230629/spec/tennis_spec.rb
+++ tennis_20230629/spec/tennis_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe Tennis do
     {server: 1, reciever: 1, score: "15 All"},
     {server: 2, reciever: 2, score: "30 All"},
     {server: 3, reciever: 3, score: "Deuce"},
-    # {server: 4, reciever: 3, score: "Advantage Server"},
-    # {server: 4, reciever: 4, score: "Deuce"},
-    # {server: 4, reciever: 5, score: "Advantage Reciever"},
-    # {server: 4, reciever: 6, score: "Game Reciever"},
+    {server: 4, reciever: 3, score: "Advantage Server"},
+    {server: 4, reciever: 4, score: "Deuce"},
+    {server: 4, reciever: 5, score: "Advantage Reciever"},
+    {server: 4, reciever: 6, score: "Game Reciever"},
     # {server: 4, reciever: 0, score: "Game Server"},
   ].each do |args|
     it "returns #{args[:score].inspect} when Server: #{args[:server]} and Reciever: #{args[:reciever]}" do
