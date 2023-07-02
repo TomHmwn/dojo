3424d76278c6ae82d65fe58e559cd253e86b46df
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-07-02 20:15:36 +0700

    refactor winning game and receiver win test fail

--- tennis_20230629/lib/tennis.rb
+++ tennis_20230629/lib/tennis.rb
@@ -8,22 +8,9 @@ class Tennis
 
   def score(player = nil)
     increment_point(player) if player
-
-    return deuce_score if deuce?
-
-    # win a game
-    if @points.values.any? { |point| point >= 4 }
-      if @points[:server] > @points[:reciever]
-        return "#{POINT_TRANSLATOR[6]} Server"
-      elsif @points[:server] < @points[:reciever]
-        return "#{POINT_TRANSLATOR[6]} Reciever"
-      end
-    end
-
-    # normal score
-    return "#{POINT_TRANSLATOR[@points[:server]]} All" if @points[:server] == @points[:reciever]
-    return unless @points.values.all? { |point| point <= 3 }
-    "#{POINT_TRANSLATOR[@points[:server]]} #{POINT_TRANSLATOR[@points[:reciever]]}"
+    return deuce_and_adv_score if match_point?
+    return winning_score if winning?
+    normal_score
   end
 
   private
@@ -32,17 +19,37 @@ class Tennis
     @points[player] += 1
   end
 
-  def deuce?
+  def match_point?
     @points.values.all? {|point| point >= 3}
   end
 
-  def deuce_score
+  def deuce_and_adv_score
     if @points[:server] == @points[:reciever]
       POINT_TRANSLATOR[4]
     elsif @points[:server] == @points[:reciever] + 1
       "#{POINT_TRANSLATOR[5]} Server"
     elsif @points[:reciever] == @points[:server] + 1
-      "#{POINT_TRANSLATOR[5]} Receiver"
+      "#{POINT_TRANSLATOR[5]} Reciever"
+    end
+  end
+
+  def winning?
+    @points.values.any? { |point| point >= 4}
+  end
+
+  def winning_score
+    if @points[:server] > @points[:reciever]
+      "#{POINT_TRANSLATOR[6]} Server"
+    elsif @points[:reciever] > @points[:server]
+      "#{POINT_TRANSLATOR[6]} Reciever"
+    end
+  end
+
+  def normal_score
+    if @points[:server] == @points[:reciever]
+      "#{POINT_TRANSLATOR[@points[:server]]} All"
+    elsif @points.values.all? { |point| point <= 3 }
+      "#{POINT_TRANSLATOR[@points[:server]]} #{POINT_TRANSLATOR[@points[:reciever]]}"
     end
   end
 end
