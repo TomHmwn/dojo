370255be64c3a84cc27d407c6fceb2d84f1f337e
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-07-06 00:30:52 +0700

    can check for 1-8 living neighbouring cells
    refactor
         build_array_for_grid
         also renamed it to initializeGridWithDeadCells

--- game_of_life_20230705/lib/game-of-life.rb
+++ game_of_life_20230705/lib/game-of-life.rb
@@ -1,24 +1,51 @@
-
-class LifeBoard
+class GameOfLife
+  DEADCELL = "."
+  LIVINGCELL = "#"
   attr_accessor :no_columns, :no_rows, :grid_array
 
   def initialize(no_rows = 3, no_columns = 3)
     @no_rows = no_rows
     @no_columns = no_columns
+    initializeGridWithDeadCells
   end
 
-  # def build_array_for_grid
-  #   build_columns = Array.new
-  #   i = 0
-  #   while i <no_columns do
-  #     random_life = =rand(0..100)
-  #     build_columns << Cell.new(random_life)
-  #   end
-  #   @grid_array = Array.new
-  #   no_rows.times do
-  #     @grid_array << build_columns
-  #   end
-  # end
+  def initializeGridWithDeadCells
+    @grid_array = Array.new(no_rows) do
+      build_columns = Array.new(no_columns, DEADCELL)
+    end
+  end
 
+  def setLivingCell(*points)
+    points.each do |point|
+      @grid_array[point[0]][point[1]] = LIVINGCELL
+    end
+  end
 
+  def countLivingNeighbourCell(row, column)
+    cellsToCheck = [
+      [row - 1, column - 1],
+      [row - 1, column],
+      [row - 1, column + 1],
+      [row, column + 1],
+      [row + 1, column + 1],
+      [row + 1, column],
+      [row + 1, column  - 1],
+      [row, column - 1],
+  ]
+    livingneighbours = 0
+    cellsToCheck.each do |cell|
+      if cellInGrid(cell[0],cell[1]) && Alive?(cell[0],cell[1])
+        livingneighbours += 1
+      end
+    end
+    livingneighbours
+  end
+
+  def cellInGrid(row, col)
+    @grid_array[row][col].class == String
+  end
+
+  def Alive?(row,col)
+    @grid_array[row][col] == "#"
+  end
 end
