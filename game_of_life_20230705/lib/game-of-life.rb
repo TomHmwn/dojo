9bae7b313c5a82fe8958c80f8a26f14eb53390d8
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-07-06 03:36:18 +0700

    be able to compute next generation
    create methods for
       underpopulation, overcrowding, willLive, and check
       if cell is dead, also fix cellinGrid to catch if value is
       not negative
       computeNextGeneration -- perform all the cases and check if,
       cell should live or die

--- game_of_life_20230705/lib/game-of-life.rb
+++ game_of_life_20230705/lib/game-of-life.rb
@@ -1,23 +1,25 @@
 class GameOfLife
-  DEADCELL = "."
-  LIVINGCELL = "#"
-  attr_accessor :no_columns, :no_rows, :grid_array
+  DEAD_CELL = "."
+  LIVING_CELL = "#"
+  attr_accessor :no_columns, :no_rows, :life_board
 
   def initialize(no_rows = 3, no_columns = 3)
     @no_rows = no_rows
     @no_columns = no_columns
-    initializeGridWithDeadCells
+    @life_board = initializeGridWithDeadCells
   end
 
   def initializeGridWithDeadCells
-    @grid_array = Array.new(no_rows) do
-      build_columns = Array.new(no_columns, DEADCELL)
+    new_board = Array.new(no_rows) do
+      build_columns = Array.new(no_columns, DEAD_CELL)
+      build_columns
     end
+    new_board
   end
 
   def setLivingCell(*points)
     points.each do |point|
-      @grid_array[point[0]][point[1]] = LIVINGCELL
+      @life_board[point[0]][point[1]] = LIVING_CELL
     end
   end
 
@@ -34,7 +36,7 @@ class GameOfLife
   ]
     livingneighbours = 0
     cellsToCheck.each do |cell|
-      if cellInGrid(cell[0],cell[1]) && Alive?(cell[0],cell[1])
+      if cellInGrid(cell[0],cell[1]) && alive?(cell[0],cell[1])
         livingneighbours += 1
       end
     end
@@ -42,10 +44,52 @@ class GameOfLife
   end
 
   def cellInGrid(row, col)
-    @grid_array[row][col].class == String
+    row_valid = row >= 0 && row < @no_rows
+    col_valid = col >= 0 && col < @no_columns
+    row_valid && col_valid && @life_board[row][col].class == String
   end
 
-  def Alive?(row,col)
-    @grid_array[row][col] == "#"
+  def alive?(row,col)
+    @life_board[row][col] == "#"
+  end
+
+  def dead?(row,col)
+    @life_board[row][col] == "."
+  end
+
+  def underpopulation?(row,col)
+    countLivingNeighbourCell(row, col) < 2
+  end
+
+  def overcrowding?(row,col)
+    countLivingNeighbourCell(row, col) > 3
+  end
+
+  def willLive?(row, col)
+    (countLivingNeighbourCell(row, col) == 3 || countLivingNeighbourCell(row, col) == 2) && alive?(row,col)
+  end
+
+  def resurrect?(row, col)
+    countLivingNeighbourCell(row, col) == 3 && dead?(row,col)
+  end
+
+  def computeNextGeneration
+    nextBoard = initializeGridWithDeadCells
+    @life_board.each_with_index do |row, row_index|
+      row.each_with_index do |cell, column_index|
+        if underpopulation?(row_index, column_index)
+          nextBoard[row_index][column_index] = DEAD_CELL
+        elsif overcrowding?(row_index, column_index)
+          nextBoard[row_index][column_index] = DEAD_CELL
+        elsif willLive?(row_index, column_index)
+          nextBoard[row_index][column_index] = LIVING_CELL
+        elsif resurrect?(row_index, column_index)
+          nextBoard[row_index][column_index] = LIVING_CELL
+        else
+          nextBoard[row_index][column_index] = @life_board[row_index][column_index]
+        end
+      end
+    end
+    @life_board = nextBoard
   end
 end
